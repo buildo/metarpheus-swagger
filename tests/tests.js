03a312b6bbf05c70d946c9237360f123380b41c5
Gabriele Petronella <gabriele@buildo.io>
2016-02-24 16:41:30 +0100

    Fix typo in tests

--- tests/tests.js
+++ tests/tests.js
@@ -10,7 +10,7 @@ test('it should produce a correct JSON output', t => {
   t.same(swaggerSpec, expectedJSON);
 })
 
-test('it should produce a correct JSON output', t => {
+test('it should produce a correct YAML output', t => {
   const expectedYAML = require('fs').readFileSync('./fixtures/swagger.yaml', 'utf8');
   const swaggerSpec = morpheusToSwagger(input);
   t.same(YAML.stringify(swaggerSpec), expectedYAML);
